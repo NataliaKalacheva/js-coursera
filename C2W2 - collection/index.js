92a2656be490e4249c8736b1bc8c23881282dfb8
NataliaKalacheva <kalachevan615@gmail.com>
2020-02-06 19:09:44 +0300

    finish task

--- C2W2 - collection/index.js
+++ C2W2 - collection/index.js
@@ -12,16 +12,34 @@ function Collection() {
 Collection.prototype.count = function() {
   return this.collection.length;
 };
+
 Collection.prototype.values = function() {
   return this.collection;
 };
+
 Collection.prototype.append = function(el) {
   if (Array.isArray(el)) {
-    console.log(this.collection);
     this.collection = this.collection.concat(el);
+    return;
+  }
+  if (el instanceof Collection) {
+    this.collection = this.collection.concat(el.collection);
+    return;
   } else {
     this.collection.push(el);
+    return;
   }
+};
+
+Collection.prototype.at = function(num) {
+  if (num <= 0 || num > this.collection.length) return null;
+  return this.collection[num - 1];
+};
+
+Collection.prototype.removeAt = function(num) {
+  if (num <= 0 || num > this.collection.length) return false;
+  this.collection.splice(num - 1, 1);
+  return true;
 };
 
 // другие методы
