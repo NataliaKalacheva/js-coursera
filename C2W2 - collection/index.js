7a4d81e1dd060513413d64260061e7a0c732ccfa
NataliaKalacheva <kalachevan615@gmail.com>
2020-02-06 15:00:24 +0300

    add constructor, methods - from, count, values, append

--- C2W2 - collection/index.js
+++ C2W2 - collection/index.js
@@ -4,15 +4,34 @@ module.exports = Collection;
  * Конструктор коллекции
  * @constructor
  */
-function Collection() {}
-
+function Collection() {
+  this.collection = [];
+}
 
 // Методы коллекции
-Collection.prototype.values = function () {};
-// другие методы
+Collection.prototype.count = function() {
+  return this.collection.length;
+};
+Collection.prototype.values = function() {
+  return this.collection;
+};
+Collection.prototype.append = function(el) {
+  if (Array.isArray(el)) {
+    console.log(this.collection);
+    this.collection = this.collection.concat(el);
+  } else {
+    this.collection.push(el);
+  }
+};
 
+// другие методы
 
 /**
  * Создание коллекции из массива значений
  */
-Collection.from = function () {};
+
+Collection.from = function(arr) {
+  let obj = new Collection();
+  obj.collection = arr;
+  return obj;
+};
