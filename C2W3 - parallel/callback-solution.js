6cdbfa64da9f53ac8f206dcff7dae254012cd933
NataliaKalacheva <kalachevan615@gmail.com>
2020-02-10 11:18:41 +0300

    solution using callback

---
+++ C2W3 - parallel/callback-solution.js
@@ -0,0 +1,30 @@
+module.exports = function(operations, callback) {
+  if (operations.length === 0) {
+    callback(null, []);
+  }
+
+  var result = [];
+  var doneOperations = 0;
+  var hasError = false;
+
+  operations.forEach(function(operation, index) {
+    operation(function next(err, data) {
+      if (hasError) {
+        return;
+      }
+
+      if (err) {
+        callback(err);
+        hasError = true;
+        return;
+      }
+
+      result[index] = data;
+      doneOperations++;
+
+      if (doneOperations === operations.length) {
+        callback(null, result);
+      }
+    });
+  });
+};
